f61106e04919f61b85115fbe66b5dddaf7a9b2ed
hoonyoung1224 <bhyhy1224@gmail.com>
2022-06-23 14:55:53 +0900

    SelfCheck Foundation 단계별 상세 설명 추가

--- src/main/java/Hoon/SelfCheckTest.java
+++ src/main/java/Hoon/SelfCheckTest.java
@@ -17,9 +17,16 @@ public class SelfCheckTest {
         dayWeight = sc.nextInt();
 
         System.out.println("오늘 활동량 단계를 입력하세요. (1~5단계)");
-        System.out.println("1단계 걷기 || 2단계 간단한 산책 || 3단계 조깅 || 4단계 || 5단계");
+        System.out.println("1단계 아주 가볍게 || 2단계 조금 가볍게  || 3단계 보통 || 4단계 조금 강하게 || 5단계 아주 강하게");
+        System.out.println("자세한 설명을 원하시면 Y를 눌러주세요.");
+        sc.nextLine();
+        if(sc.nextLine().equals("Y")){
+            System.out.println("1단계 호흡의 변화 없음 (스트레칭)|| 2단계 숨이 깊어지지만 편히 대화 가능 (걷기)|| 3단계 대화하기엔 숨쉬기 다소 힘든정도 (적당한 달리기)");
+            System.out.println("4단계 숨이 많이 차고 숨쉬기 힘든 정도 (빠른 달리기)|| 5단계 숨쉬기 매우 힘들고 극도록 힘이 드는 정도 (전력 달리기)");
+        }
 
         while (true) {
+
             dayEnergy = sc.nextInt();
             if (dayEnergy == 1) {
                 System.out.println("1번을 입력하셨습니다.");
@@ -44,6 +51,7 @@ public class SelfCheckTest {
         System.out.println(dayKcal);
         System.out.println(dayWeight);
         System.out.println(dayEnergy);
+
 
 
     }
