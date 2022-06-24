f13d7a7218758fcd0e79914ad440e6ba28e2f807
hoonyoung1224 <bhyhy1224@gmail.com>
2022-06-24 09:39:03 +0900

    SelfCheck Foundation 반복문 오류 처리

--- src/main/java/Hoon/SelfCheckTest.java
+++ src/main/java/Hoon/SelfCheckTest.java
@@ -18,17 +18,19 @@ public class SelfCheckTest {
 
         System.out.println("오늘 활동량 단계를 입력하세요. (1~5단계)");
         System.out.println("1단계 아주 가볍게 || 2단계 조금 가볍게  || 3단계 보통 || 4단계 조금 강하게 || 5단계 아주 강하게");
-        System.out.println("자세한 설명을 원하시면 Y를 눌러주세요.");
+        System.out.println("자세한 설명을 원하시면 0번을 눌러주세요.");
         sc.nextLine();
-        if(sc.nextLine().equals("Y")){
-            System.out.println("1단계 호흡의 변화 없음 (스트레칭)|| 2단계 숨이 깊어지지만 편히 대화 가능 (걷기)|| 3단계 대화하기엔 숨쉬기 다소 힘든정도 (적당한 달리기)");
-            System.out.println("4단계 숨이 많이 차고 숨쉬기 힘든 정도 (빠른 달리기)|| 5단계 숨쉬기 매우 힘들고 극도록 힘이 드는 정도 (전력 달리기)");
-        }
+
+
 
         while (true) {
 
             dayEnergy = sc.nextInt();
-            if (dayEnergy == 1) {
+            if(dayEnergy == 0){
+                    System.out.println("1단계 호흡의 변화 없음 (스트레칭)|| 2단계 숨이 깊어지지만 편히 대화 가능 (걷기)|| 3단계 대화하기엔 숨쉬기 다소 힘든정도 (적당한 달리기)");
+                    System.out.println("4단계 숨이 많이 차고 숨쉬기 힘든 정도 (빠른 달리기)|| 5단계 숨쉬기 매우 힘들고 극도록 힘이 드는 정도 (전력 달리기)");
+                    System.out.println("이제 알맞는 번호를 선택해주세요.");
+            } else if (dayEnergy == 1) {
                 System.out.println("1번을 입력하셨습니다.");
                 break;
             } else if (dayEnergy == 2) {
