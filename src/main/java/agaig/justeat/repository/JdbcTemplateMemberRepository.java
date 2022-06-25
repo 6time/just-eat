1354d6015df43c3d41aa33f49706cec72463f91c
pgrrr <pgrrr119@gmail.com>
2022-06-25 18:24:31 +0900

    ⭐ feat: AppConfig.java 스프링 빈 자동 등록 설정

--- src/main/java/agaig/justeat/repository/JdbcTemplateMemberRepository.java
+++ src/main/java/agaig/justeat/repository/JdbcTemplateMemberRepository.java
@@ -6,13 +6,14 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-
+@Repository
 public class JdbcTemplateMemberRepository implements MemberRepository{
     private final JdbcTemplate jdbcTemplate;
 
