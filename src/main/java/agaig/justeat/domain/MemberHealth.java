1db2c7a6a0d18926e716a5f34d264b2fd72a2e50
게스트 <@DESKTOP-114PMQD>
2022-06-28 15:28:03 +0900

    MemberHelth 객체 추가

--- src/main/java/agaig/justeat/domain/MemberHealth.java
+++ src/main/java/agaig/justeat/domain/MemberHealth.java
@@ -2,12 +2,16 @@ package agaig.justeat.domain;
 
 public class MemberHealth {
     private Long member_id;
-    private String height;
-    private String weight;
-    private String goal;
-    private String protein;
-    private String carbo;
-    private String fat;
+    private Double height;
+    private Double weight;
+    private Double goal;
+    private String exercise;
+    private Double bmr;
+    private Double amr;
+    private Double kcal;
+    private Double protein;
+    private Double carbo;
+    private Double fat;
 
     public Long getMember_id() {
         return member_id;
@@ -17,51 +21,83 @@ public class MemberHealth {
         this.member_id = member_id;
     }
 
-    public String getHeight() {
+    public Double getHeight() {
         return height;
     }
 
-    public void setHeight(String height) {
+    public void setHeight(Double height) {
         this.height = height;
     }
 
-    public String getWeight() {
+    public Double getWeight() {
         return weight;
     }
 
-    public void setWeight(String weight) {
+    public void setWeight(Double weight) {
         this.weight = weight;
     }
-
-    public String getGoal() {
+    
+    public Double getGoal() {
         return goal;
     }
 
-    public void setGoal(String goal) {
+    public void setGoal(Double goal) {
         this.goal = goal;
     }
 
-    public String getProtein() {
+    public String getExercise() {
+        return exercise;
+    }
+
+    public void setExercise(String exercise) {
+        this.exercise = exercise;
+    } 
+    
+    public Double getBmr() {
+        return bmr;
+    }
+
+    public void setBmr(Double bmr) {
+        this.bmr = bmr;
+    }
+    
+    public Double getAmr() {
+        return amr;
+    }
+
+    public void setAmr(Double amr) {
+        this.amr = amr;
+    }
+    
+    public Double getKcal() {
+        return kcal;
+    }
+
+    public void setKcal(Double kcal) {
+        this.kcal = kcal;
+    }
+
+    public Double getProtein() {
         return protein;
     }
 
-    public void setProtein(String protein) {
+    public void setProtein(Double protein) {
         this.protein = protein;
     }
 
-    public String getCarbo() {
+    public Double getCarbo() {
         return carbo;
     }
 
-    public void setCarbo(String carbo) {
+    public void setCarbo(Double carbo) {
         this.carbo = carbo;
     }
 
-    public String getFat() {
+    public Double getFat() {
         return fat;
     }
 
-    public void setFat(String fat) {
+    public void setFat(Double fat) {
         this.fat = fat;
     }
 }
