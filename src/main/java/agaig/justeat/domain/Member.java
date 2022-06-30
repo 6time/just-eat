a53bd9b80f3e61d4510a708aee0829f30c4ecf55
pgrrr <pgrrr119@gmail.com>
2022-06-30 15:25:34 +0900

    ⭐ feat: Member.java lombok 적용

--- src/main/java/agaig/justeat/domain/Member.java
+++ src/main/java/agaig/justeat/domain/Member.java
@@ -1,5 +1,12 @@
 package agaig.justeat.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Member {
     private Long member_id;
     private String email;
@@ -7,106 +14,17 @@ public class Member {
     private String name;
     private Long phone;
     private String address;
-    private String height;
-    private String weight;
     private String gender;
     private String birth;
-    private String goal;
-    private String exercise;
 
-    public Long getMember_id() {
-        return member_id;
-    }
-
-    public void setMember_id(Long member_id) {
+    public Member(Long member_id, String email, String password, String name, Long phone, String address, String gender, String birth) {
         this.member_id = member_id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
-    }
-
-    public Long getPhone() {
-        return phone;
-    }
-
-    public void setPhone(Long phone) {
         this.phone = phone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
         this.address = address;
-    }
-
-    public String getHeight() {
-        return height;
-    }
-
-    public void setHeight(String height) {
-        this.height = height;
-    }
-
-    public String getWeight() {
-        return weight;
-    }
-
-    public void setWeight(String weight) {
-        this.weight = weight;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
         this.gender = gender;
-    }
-
-    public String getBirth() {
-        return birth;
-    }
-
-    public void setBirth(String birth) {
         this.birth = birth;
-    }
-
-    public String getGoal() {
-        return goal;
-    }
-
-    public void setGoal(String goal) {
-        this.goal = goal;
-    }
-
-    public String getExercise() {
-        return exercise;
-    }
-
-    public void setExercise(String exercise) {
-        this.exercise = exercise;
     }
 }
