8b1caad4427e0e5b552b17a42398e43ae004b27a
pgrrr <pgrrr119@gmail.com>
2022-07-15 12:10:34 +0900

    ⭐ feat: 로그인시 Cookie 에 member_id 값이 추가 되도록 구현

--- src/main/java/agaig/justeat/controller/MemberController.java
+++ src/main/java/agaig/justeat/controller/MemberController.java
@@ -4,6 +4,7 @@ import agaig.justeat.dto.MemberResponseDto;
 import agaig.justeat.dto.MemberSaveRequestDto;
 import agaig.justeat.service.MemberService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/members")
@@ -30,14 +32,22 @@ public class MemberController {
 
     @PostMapping("")
     public String postSignIn(String email, String password, boolean rememberId, String toURL, HttpServletRequest request, HttpServletResponse response) {
+
         MemberResponseDto responseDto = memberService.signIn(email, password);
-        Cookie cookie = new Cookie("email", email);
+
+        Cookie memberCookie = new Cookie("member_id", responseDto.getMember_id().toString());
+        memberCookie.setValue(responseDto.getMember_id().toString());
+        response.addCookie(memberCookie);
+
+        Cookie idCookie = new Cookie("email", email);
         if (!rememberId) {
-            cookie.setMaxAge(0);
+            idCookie.setMaxAge(0);
         }
-        response.addCookie(cookie);
+        response.addCookie(idCookie);
+
         HttpSession session = request.getSession();
         session.setAttribute("session", responseDto);
+
         toURL = toURL == null || toURL.equals("") ? "/" : toURL;
         return "redirect:" + toURL;
     }
