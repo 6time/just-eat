5f7f711abfd58371b3c6ece45cd2f7c222952e6d
pgrrr <pgrrr119@gmail.com>
2022-07-16 21:37:31 +0900

    ♻️ refactor: MyBatis 리팩터링

--- src/main/java/agaig/justeat/domain/Member.java
+++ src/main/java/agaig/justeat/domain/Member.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,6 +19,19 @@ public class Member {
     private String address;
     private String gender;
     private String birth;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return member_id.equals(member.member_id) && email.equals(member.email) && password.equals(member.password) && name.equals(member.name) && Objects.equals(phone, member.phone) && Objects.equals(address, member.address) && Objects.equals(gender, member.gender) && Objects.equals(birth, member.birth);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(member_id, email, password, name, phone, address, gender, birth);
+    }
 
     @Builder
     public Member(Long member_id, String email, String password, String name, String phone, String address, String gender, String birth) {
