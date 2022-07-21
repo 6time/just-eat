fee8a2f710a8c4b0a38652bf256994c43ef62927
youjin931217 <youjincjstk@naver.com>
2022-07-21 10:43:41 +0900

    'health' -> 'diets' 로 'health_flag' 변경

--- sql/just-eat.sql
+++ sql/just-eat.sql
@@ -11,7 +11,6 @@
 
 CREATE TABLE `health` (
 	`member_id`	BIGINT	NOT NULL PRIMARY KEY,
-	'health_flag' BOOLEAN NOT NULL DEFAULT 0,
 	`height` DOUBLE(4,1)	NULL,
 	`weight` DOUBLE(4,1)	NULL,
 	`goal` DOUBLE(4,1)	NULL,
@@ -27,6 +26,7 @@ CREATE TABLE `diets` (
 	`diet_id`	BIGINT	NOT NULL,
 	`member_id`	BIGINT	NOT NULL,
 	`product_id`	BIGINT	NOT NULL,
+	'health_flag'   BOOLEAN NOT NULL DEFAULT 0,
 	`day_of_week`	INT	NOT NULL
 );
 
